8cbaaebb3318ffc60273fbdb1e76459df8979fd0
Gabriel Teixeira Charles da Silva <63520287+hyperGT@users.noreply.github.com>
2024-09-10 09:14:20 -0300

    Update listarDisciplina.php

--- Exercicio04-Disciplina/listarDisciplina.php
+++ Exercicio04-Disciplina/listarDisciplina.php
@@ -1,15 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>Listar Disciplinas</h1>
+<table>
+    <tr><th>nome</th><th>sigla</th><th>carga</th></tr>
 <?php
-
-$file_nm = "disciplina.txt";
-
-$myfile = fopen($file_nm, 'r') or die("Erro ao abrir arquivo");
-
-echo "<tr>";
-
-while(!feof($myfile)){    
-    echo fgets($myfile) . "<br>";
-}
+   $arqDisc = fopen("disciplinas.txt","r") or die("erro ao abrir arquivo");
  
-echo "</tr>";
-
-fclose($myfile);
+   while(!feof($arqDisc)) {
+        $linha = fgets($arqDisc);
+        $colunaDados = explode(";", $linha);
+ 
+ // <tr><td><?php echo $colunaDados[0] </td>
+        echo "<tr><td>" . $colunaDados[0] . "</td>" .
+            "<td>" . $colunaDados[1] . "</td>" .
+            "<td>" . $colunaDados[2] . "</td></tr>";
+    }
+ 
+   fclose($arqDisc);
+    $msg = "Deu tudo certo!!!";
+?>
+</table>
+<p><?php echo $msg ?></p>
+<br>
+</body>
+</html>
